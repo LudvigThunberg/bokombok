87bd0f13f3c7089a5acc4940048fed40640bdf79
LudvigThunberg <ludvigthunberg@gmail.com>
2022-03-14 13:27:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+require("./mongoose.js");
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+
+// ==== CONFIG ====
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
+);
+
+app.set("view engine", "hbs");
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+
+// === ROUTES ===
+
+// Home
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// === 404 ===
+
+app.use("/", (req, res) => {
+  res.sendStatus(404);
+});
+
+//=== LISTEN ===
+app.listen(6060, () => {
+  console.log("http://localhost:6060/");
+});
